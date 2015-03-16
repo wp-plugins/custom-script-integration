ee0fd5b4e21322f002f86c44ba15a7999458156e
wpwox <wpwox@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-16 15:30:38 +0000

    Version Update to 1.1
    git-svn-id: https://plugins.svn.wordpress.org/custom-script-integration/tags/1.1@1114009 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,78 @@
+=== Custom Script Integration ===
+Contributors: wpwox
+Donate link: http://www.wpwox.com/make-a-donation/
+Tags: google adwords conversion, custom style, custom css, google tracking, google ads, script integration, embed, plugin, wpwox, google adwords, google adsense
+Requires at least: 3
+Tested up to: 4.1.1
+Stable tag: 1.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This plugin provides custom meta box to add Google Adword conversion, ads, custom css and scripts in individual pages/posts.
+
+== Description ==
+
+This plugin provides custom meta boxes in default Pages and Posts of wordpress where you can enter any scripts for each respective pages or posts, for example:
+
+1. Google Adwords conversion tracking
+2. Google Adsense ads
+3. Any Javascripts
+4. Page/post specific CSS codes
+5. Normal HTML codes
+6. Unlimited possibilites
+
+You will get a meta box under each of page and post. The Meta box will have 4 Text Boxes, in each section you can add codes for:
+
+1. Before Content Field
+2. Below Content Field
+3. Scripts to add in <head> tag
+4. Scripts to add in footer below </body> tag
+
+Plugin Documentation:
+http://www.wpwox.com/wp-plugin/custom-script-integration/
+
+
+
+
+== Installation ==
+
+1. There are two ways to install the plugin:
+   a) Use the 'automatic install from the Wordpress plugin directory' option in the
+      Wordpress admin panel. Search for "Custom Script Integration"
+   OR
+   b) Download the zip-file, and use the 'upload' option in the Wordpress > admin panel > plugins > new plugin > upload plugin
+
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+Go to edit of any post or page, below the Content area should appear a 'Custom Script Integration' box. Simply enter your script in your required fields to execute theme in <head> tag, before </body> tag in footer, above or below contents.
+
+
+== Frequently Asked Questions ==
+
+= Can I use css in the meta boxes? =
+
+Yes, you can use css to specific pages or posts. Please make sure you have your intitial tags and closing tags properly marked, for example:
+&lt;style&lt;
+.wrapper { 
+....
+}
+&lt;/style&lt;
+
+= Does it support custom post types? =
+
+No, it does not support custom post types, only default page and posts. But you an checkout our site to ge the advance plugin of this which supports the custom post types too. http://www.wpwox.com
+
+== Screenshots ==
+
+1. '/assets/screenshot-1.jpg'
+    
+== Changelog ==
+
+= 1.0 =
+* Initial release version
+
+
+== Upgrade Notice ==
+
+= 1.0 =
+We will be informing about this later.
---
+++ wpwox-script-integration.php
@@ -0,0 +1,143 @@
+<?php
+/*
+Plugin Name: Custom Script Integration
+Plugin URI: http://www.wpwox.com
+Description: Provides custom meta boxes to add Google Adwords conversion, tracking, ads etc scripts in individual pages or posts in <head> tag, before </body> tag, above or below contents.
+Version: 1.1
+Author: WP WOX
+Author URI: http://www.wpwox.com
+
+Copyright 2015 WpWox
+
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+//Get the plugin location.
+define( 'WPWOXCUSTOMSCRIPTINTEGRATION_VERSION', '1.0' );
+define( 'WPWOXCUSTOMSCRIPTINTEGRATION__PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define( 'WPWOXCUSTOMSCRIPTINTEGRATION__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+
+//**Hook the_content to output the scripts added to the page or post.
+
+  
+add_filter('the_content', 'wpwoxcustomscript_display_hook',  10);
+
+add_action('wp_head', 'wpwoxcustomscript_display_hook_header');
+add_action('wp_footer', 'wpwoxcustomscript_display_hook_footer');
+
+
+	
+// execute the scripts on page and single posts
+function wpwoxcustomscript_display_hook_header() {
+  global $post;
+    if(is_single() || is_page()) {
+echo html_entity_decode(get_post_meta($post->ID, '_wpwoxcustomscriptcontentinhead', true));
+}
+return;
+}
+
+function wpwoxcustomscript_display_hook_footer() {
+  global $post;
+    if(is_single() || is_page()) {
+echo html_entity_decode(get_post_meta($post->ID, '_wpwoxcustomscriptcontentinfooter', true));
+}
+return;
+}
+
+
+function wpwoxcustomscript_display_hook($content='') {
+	global $post;
+  $contents=$content;
+    if(is_single() || is_page()) {
+   $contents= html_entity_decode(get_post_meta($post->ID, '_wpwoxcustomscriptcontenttop', true)) . $content . html_entity_decode(get_post_meta($post->ID, '_wpwoxcustomscriptcontentbottom', true));
+     }
+   
+return $contents;
+} 
+
+
+
+
+//Displays a box that allows users to insert the scripts for the post or page
+function wpwoxcustomscript_metaboxs($post) {
+  // Use nonce for verification
+  wp_nonce_field( plugin_basename( __FILE__ ), 'wpwox_noncename' );
+
+	?>
+  <label for="wpwoxcustomscript_content_top"><?php _e('Scripts to be inserted at the top of content','wpwoxcustomscript') ?></label><br />
+  <textarea style="width:100%; min-height: 50px;" id="wpwoxcustomscript_content_top" name="wpwoxcustomscript_content_top" /><?php echo html_entity_decode(get_post_meta($post->ID,'_wpwoxcustomscriptcontenttop',true)); ?></textarea><br />
+  <label for="wpwoxcustomscript_content_bottom"><?php _e('Scripts to be inserted at the bottom of content','wpwoxcustomscript') ?></label><br />
+  <textarea style="width:100%; min-height: 50px;" id="wpwoxcustomscript_content_bottom" name="wpwoxcustomscript_content_bottom" /><?php echo html_entity_decode(get_post_meta($post->ID,'_wpwoxcustomscriptcontentbottom',true)); ?></textarea><br />
+  <label for="wpwoxcustomscriptcontentinhead"><?php _e('Scripts to be inserted at the top in <strong>&lt;head&gt; tag</strong>','wpwoxcustomscript') ?></label><br />
+  <textarea style="width:100%; min-height: 50px;" id="wpwoxcustomscriptcontentinhead" name="wpwoxcustomscriptcontentinhead" /><?php echo html_entity_decode(get_post_meta($post->ID,'_wpwoxcustomscriptcontentinhead',true)); ?></textarea><br />
+  <label for="wpwoxcustomscriptcontentinfooter"><?php _e('Scripts to be inserted at the bottom <strong>before &lt;/body&gt;</strong>','wpwoxcustomscript') ?></label><br />
+  <textarea style="width:100%; min-height: 50px;" id="wpwoxcustomscriptcontentinfooter" name="wpwoxcustomscriptcontentinfooter" /><?php echo html_entity_decode(get_post_meta($post->ID,'_wpwoxcustomscriptcontentinfooter',true)); ?></textarea>
+<p style="color:blue; text-align:right; font-weight: bold;">Get a <a href="http://www.wpwox.com/custom-script-integration-pro/" style="color:#ff0000" target="_blank">Pro Custom Script Integration Plugin</a> for more options and Custom Post Type support</p>
+  	<?php
+}
+
+
+
+
+//Add the meta box to post and page 
+function wpwox_custom_script_meta_box() {
+	add_meta_box('wpwox_custom_script','WPWOX Custom Script Integration','wpwoxcustomscript_metaboxs','post','advanced');
+	add_meta_box('wpwox_custom_script','WPWOX Custom Script Integration','wpwoxcustomscript_metaboxs','page','advanced');
+}
+add_action('admin_menu', 'wpwox_custom_script_meta_box');
+
+// When the post is updating, save the script.
+
+function wpwoxcustomscript_updates($pID) {
+
+  // if the function is called by the WP autosave feature, nothing must be saved
+  if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE )
+    return;
+    
+  // verify this came from the our screen and with proper authorization,
+  // because save_post can be triggered at other times
+
+  if ( !wp_verify_nonce( $_POST['wpwox_noncename'], plugin_basename( __FILE__ ) ) )
+      return;
+
+  
+
+  if ( 'page' == $_POST['post_type'] ) 
+  {
+    if ( !current_user_can( 'edit_page', $pID ) )
+        return;
+  }
+  else
+  {
+    if ( !current_user_can( 'edit_post', $pID ) )
+        return;
+  }
+
+  // update the meta datas here
+  $text = (isset($_POST['wpwoxcustomscript_content_top'])) ? $_POST['wpwoxcustomscript_content_top'] : '';
+ $text= str_replace("\n", '', $text);
+  $text= esc_js($text);
+  update_post_meta($pID, '_wpwoxcustomscriptcontenttop', $text);
+
+  $text = (isset($_POST['wpwoxcustomscript_content_bottom'])) ? $_POST['wpwoxcustomscript_content_bottom'] : '';
+   $text= str_replace("\n", '', $text);
+  $text= esc_js($text);
+update_post_meta($pID, '_wpwoxcustomscriptcontentbottom', $text);
+
+    $text = (isset($_POST['wpwoxcustomscriptcontentinhead'])) ? $_POST['wpwoxcustomscriptcontentinhead'] : '';
+     $text= str_replace("\n", '', $text);
+  $text= esc_js($text);
+update_post_meta($pID, '_wpwoxcustomscriptcontentinhead', $text);
+  
+  $text = (isset($_POST['wpwoxcustomscriptcontentinfooter'])) ? $_POST['wpwoxcustomscriptcontentinfooter'] : '';
+ $text= str_replace("\n", '', $text);
+  $text= esc_js($text);
+  update_post_meta($pID, '_wpwoxcustomscriptcontentinfooter', $text);
+}
+add_action('save_post', 'wpwoxcustomscript_updates');
+
+?>
